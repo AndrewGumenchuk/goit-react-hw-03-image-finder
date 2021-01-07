d4fb604f254f89612a2881a167d70a8637d0c72a
AndrewGumenchuk <64306832+AndrewGumenchuk@users.noreply.github.com>
2021-01-07 18:08:07 +0200

    update

--- src/components/LoaderComponent/LoaderComponent.js
+++ src/components/LoaderComponent/LoaderComponent.js
@@ -1,6 +1,6 @@
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import s from './Loader.module.css';
+import s from './LoaderComponent.module.css';
 
 function LoaderComponent() {
   return (
